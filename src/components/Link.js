363bcb8873b4264da141dee82c853b9ccaf94999
Anna Levitskaya <anna.levitskaya@polcode.net>
2023-02-13 13:19:00 +0100

    create custom navigation hook

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,8 +1,10 @@
-import { useContext } from "react";
-import NavigationContext from "../context/navigation";
+import classNames from "classnames";
+import useNavigation from "../hooks/use-navigation";
 
 function Link({ to, children }) {
-  const { navigate } = useContext(NavigationContext);
+  const { navigate } = useNavigation();
+
+  const classes = classNames("text-blue-500");
 
   const handleClick = (event) => {
     // if user holds down the 'ctrl' ('command') key and then clicks on the link,
@@ -20,7 +22,7 @@ function Link({ to, children }) {
   };
 
   return (
-    <a href={to} onClick={handleClick}>
+    <a className={classes} href={to} onClick={handleClick}>
       {children}
     </a>
   );
