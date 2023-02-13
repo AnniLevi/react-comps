d75dd2a8df30189bc1e23eb23bb35b7712746253
Anna Levitskaya <anna.levitskaya@polcode.net>
2023-02-13 13:12:05 +0100

    create Route component that directs to the appropriate component by the given path

--- src/components/Link.js
+++ src/components/Link.js
@@ -5,13 +5,25 @@ function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
 
   const handleClick = (event) => {
+    // if user holds down the 'ctrl' ('command') key and then clicks on the link,
+    // it should be open in new tab
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
+
+    // otherwise
     // prevent browser's normal behavior of total page refresh when clicking on <a> element
     event.preventDefault();
 
     // instead we want to use navigate function from the context
     navigate(to);
   };
-  return <a onClick={handleClick}>{children}</a>;
+
+  return (
+    <a href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 }
 
 export default Link;
